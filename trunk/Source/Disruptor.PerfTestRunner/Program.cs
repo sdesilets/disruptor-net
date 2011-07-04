7232735f666002fbb5d0b78b0fa6110f94af2608
odeheurles@gmail.com <odeheurles@gmail.com@aaa950c2-1a1b-1b41-b3eb-a8d814a07a0f>
2011-07-04 11:30:48 +0000

    Fix a bug in BatchConsumer (invalid sequence number was published) and get Pipeline3StepPerfTest working refactored test runner

--- trunk/Source/Disruptor.PerfTestRunner/Program.cs
+++ trunk/Source/Disruptor.PerfTestRunner/Program.cs
@@ -7,20 +7,24 @@ namespace Disruptor.PerfTestRunner
     {
         static void Main()
         {
+            //RunTest(new UniCast1P1CPerfTest());
+            RunTest(new Pipeline3StepPerfTest());
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void RunTest(AbstractPerfTestQueueVsDisruptor test)
+        {
             try
             {
-                (new UniCast1P1CPerfTest()).ShouldCompareDisruptorVsQueues();
+                test.ShouldCompareDisruptorVsQueues();
             }
             catch (Exception e)
             {
                 Console.WriteLine("Exception thrown in UniCast1P1CPerfTest:" + e);
                 throw;
             }
-
-            //(new Pipeline3StepPerfTest()).ShouldCompareDisruptorVsQueues();
-
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
         }
     }
 }
--- trunk/Source/Disruptor.PerfTests/Pipeline3StepPerfTest.cs
+++ trunk/Source/Disruptor.PerfTests/Pipeline3StepPerfTest.cs
@@ -63,27 +63,30 @@ namespace Disruptor.PerfTests
     public class Pipeline3StepPerfTest : AbstractPerfTestQueueVsDisruptor
     {
         private const  int Size = 1024 * 32;
-        private const  long Iterations = 1000 * 1000 * 10L;
-        private const  long OperandTwoInitialValue = 777L;
+        private const long Iterations = 1000 * 1000 * 10L;
+        private static long _expectedResult;
+        private const long OperandTwoInitialValue = 777L;
+
         private static long ExpectedResult
         {
             get
             {
-                var temp = 0L;
-                var operandTwo = OperandTwoInitialValue;
-
-                for (long i = 0; i < Iterations; i++)
+                if(_expectedResult == 0)
                 {
-                    var stepOneResult = i + operandTwo--;
-                    var stepTwoResult = stepOneResult + 3;
+                    var operandTwo = OperandTwoInitialValue;
 
-                    if ((stepTwoResult & 4L) == 4L)
+                    for (long i = 0; i < Iterations; i++)
                     {
-                        ++temp;
+                        var stepOneResult = i + operandTwo--;
+                        var stepTwoResult = stepOneResult + 3;
+
+                        if ((stepTwoResult & 4L) == 4L)
+                        {
+                            ++_expectedResult;
+                        }
                     }
                 }
-
-                return temp;
+                return _expectedResult;
             }
         }
 
@@ -100,11 +103,9 @@ namespace Disruptor.PerfTests
         private readonly RingBuffer<FunctionEntry> _ringBuffer;
 
         private readonly IConsumerBarrier<FunctionEntry> _stepOneConsumerBarrier;
-        private readonly FunctionHandler _stepOneFunctionHandler;
         private readonly BatchConsumer<FunctionEntry> _stepOneBatchConsumer;
 
         private readonly IConsumerBarrier<FunctionEntry> _stepTwoConsumerBarrier;
-        private readonly FunctionHandler _stepTwoFunctionHandler;
         private readonly BatchConsumer<FunctionEntry> _stepTwoBatchConsumer;
 
         private readonly IConsumerBarrier<FunctionEntry> _stepThreeConsumerBarrier;
@@ -112,6 +113,7 @@ namespace Disruptor.PerfTests
         private readonly BatchConsumer<FunctionEntry> _stepThreeBatchConsumer;
 
         private readonly IReferenceTypeProducerBarrier<FunctionEntry> _producerBarrier;
+        
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
     
@@ -126,12 +128,10 @@ namespace Disruptor.PerfTests
                                       WaitStrategyFactory.WaitStrategyOption.Yielding);
 
             _stepOneConsumerBarrier = _ringBuffer.CreateConsumerBarrier();
-            _stepOneFunctionHandler = new FunctionHandler(FunctionStep.One);
-            _stepOneBatchConsumer = new BatchConsumer<FunctionEntry>(_stepOneConsumerBarrier, _stepOneFunctionHandler);
+            _stepOneBatchConsumer = new BatchConsumer<FunctionEntry>(_stepOneConsumerBarrier, new FunctionHandler(FunctionStep.One));
 
             _stepTwoConsumerBarrier = _ringBuffer.CreateConsumerBarrier(_stepOneBatchConsumer);
-            _stepTwoFunctionHandler = new FunctionHandler(FunctionStep.Two);
-            _stepTwoBatchConsumer = new BatchConsumer<FunctionEntry>(_stepTwoConsumerBarrier, _stepTwoFunctionHandler);
+            _stepTwoBatchConsumer = new BatchConsumer<FunctionEntry>(_stepTwoConsumerBarrier, new FunctionHandler(FunctionStep.Two));
 
             _stepThreeConsumerBarrier = _ringBuffer.CreateConsumerBarrier(_stepTwoBatchConsumer);
             _stepThreeFunctionHandler = new FunctionHandler(FunctionStep.Three);
@@ -142,11 +142,42 @@ namespace Disruptor.PerfTests
 
         protected override long RunQueuePass(int passNumber)
         {
+            _stepThreeQueueConsumer.Reset();
+
+            ThreadPool.QueueUserWorkItem(_ => _stepOneQueueConsumer.Run());
+            ThreadPool.QueueUserWorkItem(_ => _stepTwoQueueConsumer.Run());
+            ThreadPool.QueueUserWorkItem(_ => _stepThreeQueueConsumer.Run());
+
+            var sw = Stopwatch.StartNew();
+
+            var operandTwo = OperandTwoInitialValue;
+            for (long i = 0; i < Iterations; i++)
+            {
+                var values = new long[2];
+                values[0] = i;
+                values[1] = operandTwo--;
+                _stepOneQueue.Add(values);
+            }
+
+            while (!_stepThreeQueueConsumer.Done)
+            {
+                // busy spin
+            }
+
+            var opsPerSecond= (Iterations * 1000L) / sw.ElapsedMilliseconds;
+
+            Assert.AreEqual(ExpectedResult, _stepThreeQueueConsumer.StepThreeCounter);
+
+            return opsPerSecond;
+        }
+
+        protected override long RunDisruptorPass(int passNumber)
+        {
             _stepThreeFunctionHandler.Reset();
 
-            ThreadPool.QueueUserWorkItem(_ => _stepOneBatchConsumer.Run());
-            ThreadPool.QueueUserWorkItem(_ => _stepTwoBatchConsumer.Run());
-            ThreadPool.QueueUserWorkItem(_ => _stepThreeBatchConsumer.Run());
+            new Thread(_stepOneBatchConsumer.Run){Name="Step 1"}.Start();
+            new Thread(_stepTwoBatchConsumer.Run) { Name = "Step 2" }.Start();
+            new Thread(_stepThreeBatchConsumer.Run) { Name = "Step 3" }.Start();
 
             var sw = Stopwatch.StartNew();
 
@@ -173,38 +204,6 @@ namespace Disruptor.PerfTests
             _stepThreeBatchConsumer.Halt();
 
             Assert.AreEqual(ExpectedResult, _stepThreeFunctionHandler.StepThreeCounter);
-        
-
-            return opsPerSecond;
-        }
-
-        protected override long RunDisruptorPass(int passNumber)
-        {
-            _stepThreeQueueConsumer.Reset();
-
-            ThreadPool.QueueUserWorkItem(_ => _stepOneQueueConsumer.Run());
-            ThreadPool.QueueUserWorkItem(_ => _stepTwoQueueConsumer.Run());
-            ThreadPool.QueueUserWorkItem(_ => _stepThreeQueueConsumer.Run());
-
-            var sw = Stopwatch.StartNew();
-
-            var operandTwo = OperandTwoInitialValue;
-            for (long i = 0; i < Iterations; i++)
-            {
-                var values = new long[2];
-                values[0] = i;
-                values[1] = operandTwo--;
-                _stepOneQueue.Add(values);
-            }
-
-            while (! _stepThreeQueueConsumer.Done)
-            {
-                // busy spin
-            }
-
-            var opsPerSecond = (Iterations * 1000L) / sw.ElapsedMilliseconds;
-
-            Assert.AreEqual(ExpectedResult, _stepThreeQueueConsumer.StepThreeCounter);
 
             return opsPerSecond;
         }
